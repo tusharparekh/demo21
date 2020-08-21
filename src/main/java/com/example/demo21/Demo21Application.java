b2999fd05841524f31ca819d9d93d2313dd70b51
TUSHAR PAREKH <tusharparekh1@gmail.com>
2020-08-21 10:46:00 +0530

    Added Hello Tushar Log

--- src/main/java/com/example/demo21/Demo21Application.java
+++ src/main/java/com/example/demo21/Demo21Application.java
@@ -8,6 +8,6 @@ public class Demo21Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo21Application.class, args);
+        System.out.println("Hello World");
     }
-
 }
